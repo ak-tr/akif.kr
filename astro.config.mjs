28a70ba22caf0d621865f5122fc7b682914b21f3
ak-tr <62529128+ak-tr@users.noreply.github.com>
2025-09-29 00:20:08 +0100

    remove highlight rehype plugin - no longer required

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,10 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 
-
 import { SITE_METADATA } from "./src/consts.ts";
 
 // Rehype plugins
-import rehypeHighlight from "rehype-highlight";
 import rehypeSlugAnchorSectionize from "rehype-slug-anchor-sectionize";
-
-import langPython from "highlight.js/lib/languages/python";
-import langJson from "highlight.js/lib/languages/json";
-import langShell from "highlight.js/lib/languages/shell";
 
 import robotsTxt from "astro-robots-txt";
 
@@ -23,13 +17,6 @@ export default defineConfig({
         mdx({
             rehypePlugins: [
                 rehypeSlugAnchorSectionize,
-                () => rehypeHighlight({
-                    languages: {
-                        python: langPython,
-                        json: langJson,
-                        sh: langShell,
-                    }
-                })
             ],
         }),
         sitemap(),
